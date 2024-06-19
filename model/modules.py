29e5a1f5d9874761224a5b6c1e081c4cdfb4ece2
globe01 <globe_NKU2172@163.com>
2024-06-20 01:02:24 +0800

    try

--- model/modules.py
+++ model/modules.py
@@ -13,7 +13,9 @@ from utils.tools import get_mask_from_lengths, pad
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
+# 本文件主要是定义Variance Adaptor
+# 其中主要包括Duration Predictor、Length Regulator、Pitch Predictor和Energy Predictor
+# 完整Variance Adaptor
 class VarianceAdaptor(nn.Module):
     """Variance Adaptor"""
 
@@ -24,6 +26,7 @@ class VarianceAdaptor(nn.Module):
         self.pitch_predictor = VariancePredictor(model_config)
         self.energy_predictor = VariancePredictor(model_config)
 
+        # 设置pitch和energy的级别
         self.pitch_feature_level = preprocess_config["preprocessing"]["pitch"][
             "feature"
         ]
@@ -33,11 +36,14 @@ class VarianceAdaptor(nn.Module):
         assert self.pitch_feature_level in ["phoneme_level", "frame_level"]
         assert self.energy_feature_level in ["phoneme_level", "frame_level"]
 
+        # 设置pitch和energy的量化方式
         pitch_quantization = model_config["variance_embedding"]["pitch_quantization"]
         energy_quantization = model_config["variance_embedding"]["energy_quantization"]
         n_bins = model_config["variance_embedding"]["n_bins"]
         assert pitch_quantization in ["linear", "log"]
         assert energy_quantization in ["linear", "log"]
+
+        # 加载pitch和energy的正则化所需参数
         with open(
             os.path.join(preprocess_config["path"]["preprocessed_path"], "stats.json")
         ) as f:
@@ -70,6 +76,7 @@ class VarianceAdaptor(nn.Module):
                 requires_grad=False,
             )
 
+        # pitch和energy的嵌入层
         self.pitch_embedding = nn.Embedding(
             n_bins, model_config["transformer"]["encoder_hidden"]
         )
@@ -77,27 +84,29 @@ class VarianceAdaptor(nn.Module):
             n_bins, model_config["transformer"]["encoder_hidden"]
         )
 
+    # 计算pitch嵌入层
     def get_pitch_embedding(self, x, target, mask, control):
-        prediction = self.pitch_predictor(x, mask)
-        if target is not None:
+        prediction = self.pitch_predictor(x, mask)# pitch预测器预测的数值
+        if target is not None:# target存在，训练过程，使用target计算embedding
             embedding = self.pitch_embedding(torch.bucketize(target, self.pitch_bins))
-        else:
-            prediction = prediction * control
+        else: # target不存在，预测过程，使用prediction计算embedding
+            prediction = prediction * control# control是用于控制的系数
             embedding = self.pitch_embedding(
                 torch.bucketize(prediction, self.pitch_bins)
             )
-        return prediction, embedding
+        return prediction, embedding# prediction用于训练过程计算损失，embedding与x相加进行后续计算
 
+    # 计算energy嵌入层
     def get_energy_embedding(self, x, target, mask, control):
-        prediction = self.energy_predictor(x, mask)
-        if target is not None:
+        prediction = self.energy_predictor(x, mask)# energy预测器预测的数值
+        if target is not None:# target存在，训练过程，使用target计算embedding
             embedding = self.energy_embedding(torch.bucketize(target, self.energy_bins))
-        else:
-            prediction = prediction * control
+        else:# target不存在，预测过程，使用prediction计算embedding
+            prediction = prediction * control# control是用于控制的系数
             embedding = self.energy_embedding(
                 torch.bucketize(prediction, self.energy_bins)
             )
-        return prediction, embedding
+        return prediction, embedding# prediction用于训练过程计算损失，embedding与x相加进行后续计算
 
     def forward(
         self,
@@ -113,29 +122,30 @@ class VarianceAdaptor(nn.Module):
         d_control=1.0,
     ):
 
-        log_duration_prediction = self.duration_predictor(x, src_mask)
+        log_duration_prediction = self.duration_predictor(x, src_mask)# 对音素序列预测的持续时间
         if self.pitch_feature_level == "phoneme_level":
             pitch_prediction, pitch_embedding = self.get_pitch_embedding(
                 x, pitch_target, src_mask, p_control
             )
-            x = x + pitch_embedding
+            x = x + pitch_embedding # 累加pitch嵌入层
         if self.energy_feature_level == "phoneme_level":
             energy_prediction, energy_embedding = self.get_energy_embedding(
                 x, energy_target, src_mask, p_control
             )
-            x = x + energy_embedding
+            x = x + energy_embedding # 累加energy嵌入层
 
-        if duration_target is not None:
-            x, mel_len = self.length_regulator(x, duration_target, max_len)
+        if duration_target is not None:# duration_target，训练过程，使用duration_target计算
+            x, mel_len = self.length_regulator(x, duration_target, max_len)# 使用duration_target调整x
             duration_rounded = duration_target
-        else:
+        else:# 预测过程
+            # 基于log_duration_prediction构建duration_rounded，用于调整x
             duration_rounded = torch.clamp(
                 (torch.round(torch.exp(log_duration_prediction) - 1) * d_control),
                 min=0,
             )
             x, mel_len = self.length_regulator(x, duration_rounded, max_len)
             mel_mask = get_mask_from_lengths(mel_len)
-
+        # 与phoneme_level一致, frame_level的pitch和energy也是在这里计算的
         if self.pitch_feature_level == "frame_level":
             pitch_prediction, pitch_embedding = self.get_pitch_embedding(
                 x, pitch_target, mel_mask, p_control
@@ -148,30 +158,32 @@ class VarianceAdaptor(nn.Module):
             x = x + energy_embedding
 
         return (
-            x,
-            pitch_prediction,
-            energy_prediction,
-            log_duration_prediction,
-            duration_rounded,
-            mel_len,
-            mel_mask,
+            x,# 经过Variance Adaptor处理后的x
+            pitch_prediction,# pitch预测值
+            energy_prediction,# energy预测值
+            log_duration_prediction,# 音素序列预测的持续时间
+            duration_rounded,# 基于log_duration_prediction构建的duration_rounded
+            mel_len,# mel的长度
+            mel_mask,# mel的mask
         )
 
-
+# 长度调整器
 class LengthRegulator(nn.Module):
     """Length Regulator"""
 
     def __init__(self):
         super(LengthRegulator, self).__init__()
 
+    # 对输入的x进行调整，使其长度与duration_target一致
     def LR(self, x, duration, max_len):
         output = list()
         mel_len = list()
         for batch, expand_target in zip(x, duration):
-            expanded = self.expand(batch, expand_target)
+            expanded = self.expand(batch, expand_target)# 对batch进行扩展
             output.append(expanded)
-            mel_len.append(expanded.shape[0])
+            mel_len.append(expanded.shape[0])# 记录扩展后的长度
 
+        # 如果传入了max_len，对output进行padding
         if max_len is not None:
             output = pad(output, max_len)
         else:
@@ -179,13 +191,14 @@ class LengthRegulator(nn.Module):
 
         return output, torch.LongTensor(mel_len).to(device)
 
+    # 对batch进行扩展
     def expand(self, batch, predicted):
         out = list()
 
         for i, vec in enumerate(batch):
-            expand_size = predicted[i].item()
-            out.append(vec.expand(max(int(expand_size), 0), -1))
-        out = torch.cat(out, 0)
+            expand_size = predicted[i].item()# 预测的持续时间
+            out.append(vec.expand(max(int(expand_size), 0), -1))# 对vec进行扩展
+        out = torch.cat(out, 0)# 拼接整个音素序列
 
         return out
 
@@ -200,11 +213,11 @@ class VariancePredictor(nn.Module):
     def __init__(self, model_config):
         super(VariancePredictor, self).__init__()
 
-        self.input_size = model_config["transformer"]["encoder_hidden"]
-        self.filter_size = model_config["variance_predictor"]["filter_size"]
-        self.kernel = model_config["variance_predictor"]["kernel_size"]
-        self.conv_output_size = model_config["variance_predictor"]["filter_size"]
-        self.dropout = model_config["variance_predictor"]["dropout"]
+        self.input_size = model_config["transformer"]["encoder_hidden"]# 输入的维度
+        self.filter_size = model_config["variance_predictor"]["filter_size"]# 卷积层的输出维度
+        self.kernel = model_config["variance_predictor"]["kernel_size"]# 卷积核的大小
+        self.conv_output_size = model_config["variance_predictor"]["filter_size"]# 卷积层的输出维度
+        self.dropout = model_config["variance_predictor"]["dropout"]# dropout的概率
 
         self.conv_layer = nn.Sequential(
             OrderedDict(
@@ -240,16 +253,16 @@ class VariancePredictor(nn.Module):
         self.linear_layer = nn.Linear(self.conv_output_size, 1)
 
     def forward(self, encoder_output, mask):
-        out = self.conv_layer(encoder_output)
-        out = self.linear_layer(out)
-        out = out.squeeze(-1)
+        out = self.conv_layer(encoder_output)# 卷积层
+        out = self.linear_layer(out)# 线性层
+        out = out.squeeze(-1)# 去掉最后一维
 
         if mask is not None:
-            out = out.masked_fill(mask, 0.0)
+            out = out.masked_fill(mask, 0.0)# mask处理
 
         return out
 
-
+# 卷积层（1维）
 class Conv(nn.Module):
     """
     Convolution Module
